6c6c6fef679cc4bb5b310d394a7e3e6f445115e1
Harsh Kumar rattewal <harshrattewal@gmail.com>
2023-09-14 15:28:19 +0530

    added database config and initialzation

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+dotenv.config();
+
+app.get("/health", (req, res) => {
+    const dbStatus =
+      mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+  
+    res.status(200).json({
+      server: "Running",
+      database: dbStatus,
+    });
+});
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+  
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res
+      .status(500)
+      .json({ error: "Something went wrong! Please try again later." });
+});
+
+app.listen(process.env.PORT, () => {
+    mongoose
+      .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("MongoDB Connected");
+        console.log(`App listening at http://localhost:${process.env.PORT}`);
+      })
+      .catch((err) => console.log(err));
+  });
+  
+module.exports = app;
